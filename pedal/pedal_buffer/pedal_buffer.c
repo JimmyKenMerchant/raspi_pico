22aca3e14deb651f03a1f39d4d148c13a8d5fd44
Kenta Ishii <k.i.drivingroad21@gmail.com>
2021-04-02 16:02:02 +0900

    Modified pedal_buffer

--- pedal/pedal_buffer/pedal_buffer.c
+++ pedal/pedal_buffer/pedal_buffer.c
@@ -12,15 +12,18 @@
 
 // Standards
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 // Dependancies
 #include "pico/stdlib.h"
+#include "pico/divider.h"
 #include "pico/multicore.h"
 #include "hardware/pwm.h"
 #include "hardware/adc.h"
 #include "hardware/irq.h"
 // raspi_pico/include
 #include "macros_pico.h"
-#include "function_generator_pico.h"
+//#include "function_generator_pico.h"
 
 #define PEDAL_BUFFER_PWM_1_GPIO 16
 #define PEDAL_BUFFER_PWM_OFFSET 2048
@@ -28,9 +31,9 @@
 #define PEDAL_BUFFER_ADC_0_GPIO 26
 #define PEDAL_BUFFER_ADC_1_GPIO 27
 #define PEDAL_BUFFER_ADC_2_GPIO 28
-#define PEDAL_BUFFER_PWM_THRESHOLD 0xF
+#define PEDAL_BUFFER_PWM_THRESHOLD 0x8F
 
-function_generator_pico* function_generator;
+//function_generator_pico* function_generator;
 
 uint32 pedal_buffer_pwm_slice_num;
 uint32 pedal_buffer_pwm_channel;
@@ -40,6 +43,7 @@ uint16 pedal_buffer_conversion_3;
 uint16 pedal_buffer_conversion_1_temp;
 uint16 pedal_buffer_conversion_2_temp;
 uint16 pedal_buffer_conversion_3_temp;
+char8 pedal_buffer_gain;
 bool pedal_buffer_is_outstanding_on_adc;
 
 void pedal_buffer_core_1();
@@ -52,11 +56,12 @@ int main(void) {
     multicore_launch_core1(pedal_buffer_core_1);
     printf("@main 1 - Let's Start!\n");
     while (true) {
-        //printf("@main 2 - pedal_buffer_conversion_1 %0x\n", multicore_fifo_pop_blocking());
-        //printf("@main 3 - pedal_buffer_conversion_2 %0x\n", multicore_fifo_pop_blocking());
-        //printf("@main 4 - pedal_buffer_conversion_3 %0x\n", multicore_fifo_pop_blocking());
+        //printf("@main 2 - pedal_buffer_conversion_1 %0x\n", pedal_buffer_conversion_1);
+        //printf("@main 3 - pedal_buffer_conversion_2 %0x\n", pedal_buffer_conversion_2);
+        //printf("@main 4 - pedal_buffer_conversion_3 %0x\n", pedal_buffer_conversion_3);
         //printf("@main 5 - debug_time %d\n", multicore_fifo_pop_blocking());
-        //sleep_ms(1000);
+        //sleep_ms(500);
+        tight_loop_contents();
     }
     return 0;
 }
@@ -111,26 +116,35 @@ void pedal_buffer_on_pwm_irq_wrap() {
     pedal_buffer_conversion_1 = pedal_buffer_conversion_1_temp;
     if (abs(pedal_buffer_conversion_2_temp - pedal_buffer_conversion_2) > PEDAL_BUFFER_PWM_THRESHOLD) {
         pedal_buffer_conversion_2 = pedal_buffer_conversion_2_temp;
+        pedal_buffer_gain = pedal_buffer_conversion_2 >> 8; // Make 4-bit Value (0-15)
     }
     if (abs(pedal_buffer_conversion_3_temp - pedal_buffer_conversion_3) > PEDAL_BUFFER_PWM_THRESHOLD) {
         pedal_buffer_conversion_3 = pedal_buffer_conversion_3_temp;
     }
-    pwm_set_chan_level(pedal_buffer_pwm_slice_num, pedal_buffer_pwm_channel, pedal_buffer_conversion_1);
-    //multicore_fifo_push_blocking(pedal_buffer_conversion_1_temp);
-    //multicore_fifo_push_blocking(pedal_buffer_conversion_2_temp);
-    //multicore_fifo_push_blocking(pedal_buffer_conversion_3_temp);
-    //multicore_fifo_push_blocking(absolute_time_diff_us(from_time, get_absolute_time()));
-    pwm_clear_irq(pedal_buffer_pwm_slice_num); // Seems Overwarp IRQ Otherwise
+    int32 normalized_1 = pedal_buffer_conversion_1 - PEDAL_BUFFER_PWM_OFFSET;
+    if (pedal_buffer_gain > 7) {
+        normalized_1 *= (pedal_buffer_gain - 7);
+    } else {
+        normalized_1 /= abs(pedal_buffer_gain - 8);
+    }
+    if (normalized_1 > PEDAL_BUFFER_PWM_PEAK) {
+        normalized_1 = PEDAL_BUFFER_PWM_PEAK;
+    } else if (normalized_1 < -PEDAL_BUFFER_PWM_PEAK) {
+        normalized_1 = -PEDAL_BUFFER_PWM_PEAK;
+    }
+    pwm_set_chan_level(pedal_buffer_pwm_slice_num, pedal_buffer_pwm_channel, (uint16)(normalized_1 + PEDAL_BUFFER_PWM_OFFSET));
+    //multicore_fifo_push_blocking(absolute_time_diff_us(from_time, get_absolute_time())); // To send a made pointer, etc.
     if (! pedal_buffer_is_outstanding_on_adc) {
         pedal_buffer_is_outstanding_on_adc = true;
         adc_select_input(0); // Ensure to Start from A0
         adc_run(true);
     }
+    pwm_clear_irq(pedal_buffer_pwm_slice_num); // Seems Overwarp IRQ Otherwise
 }
 
 void pedal_buffer_on_adc_irq_fifo() {
     adc_run(false);
-    uint16 adc_fifo_level = adc_fifo_get_level();
+    uint16 adc_fifo_level = adc_fifo_get_level(); // Seems 8 at Maximum
     //printf("@pedal_buffer_on_adc_irq_fifo 1 - adc_fifo_level: %d\n", adc_fifo_level);
     for (uint16 i = 0; i < adc_fifo_level; i++) {
         //printf("@pedal_buffer_on_adc_irq_fifo 2 - i: %d\n", i);
