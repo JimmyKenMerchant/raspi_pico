3314340d269d43a7053d36fa85e44b34e6e5b9ca
Kenta Ishii <k.i.drivingroad21@gmail.com>
2021-05-05 20:04:37 +0900

    Modified qspi_flash to Write Data Sequentially

--- qspi_flash/qspi_flash.c
+++ qspi_flash/qspi_flash.c
@@ -12,11 +12,19 @@
 
 // Standards
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 // Dependancies
 #include "pico/stdlib.h"
+#include "hardware/flash.h"
+#include "hardware/sync.h"
+#include "hardware/regs/xip.h"
+#include "hardware/structs/xip_ctrl.h"
 // raspi_pico/include
 #include "macros_pico.h"
+
+/**
+ * Caution! This Program Tries to Write Data to Flash Memory
+ */
 
 /**
  * __flash_binary_end can be watched in qspi_flash.elf.map.
@@ -32,12 +40,49 @@
     #error PICO_NO_FLASH is true.
 #endif
 
+uint32 qspi_flash_debug_time;
+
 int main(void) {
     stdio_init_all();
     sleep_ms(2000); // Wait for Rediness of USB for Messages
     // Binary End
     printf("@main 1 - &__flash_binary_end %0x\n", (intptr_t)&__flash_binary_end);
+    printf("@main 2 - xip_ctrl_hw->ctrl %0x\n", xip_ctrl_hw->ctrl);
+    hw_clear_bits(&xip_ctrl_hw->ctrl, XIP_CTRL_ERR_BADWRITE_BITS|XIP_CTRL_EN_BITS);
+    hw_set_bits(&xip_ctrl_hw->ctrl, XIP_CTRL_POWER_DOWN_BITS);
+    __dsb();
+    printf("@main 3 - xip_ctrl_hw->ctrl %0x\n", xip_ctrl_hw->ctrl);
+    uint32* binary_end = (uint32*)&__flash_binary_end;
+    printf("@main 4 - binary_end %0x\n", binary_end);
+    printf("@main 4 - *binary_end %0x\n", *binary_end);
+    binary_end -= 1;
+    printf("@main 4 - binary_end %0x\n", binary_end);
+    printf("@main 4 - *binary_end %0x\n", *binary_end);
+    binary_end += 1;
+    uint32* free_start = binary_end;
+    if ((uint32)free_start % 4096) free_start = (uint32*)((uint32)(binary_end + 1024) & 0xFFFFF000); // 4096-byte (1024 Words) Aligned Sector
+    printf("@main 6 - free_start %0x\n", free_start);
+    printf("@main 7 - *free_start %0x\n", *free_start);
+    int32 free_start_offset = (uint32)free_start - 0x10000000;
+    printf("@main 8 - free_start_offset %0x\n", free_start_offset);
+    uchar8 array_int[FLASH_PAGE_SIZE];
+    qspi_flash_debug_time = 0;
     while (true) {
-        tight_loop_contents();
+        printf("@main 9 - xip_ctrl_hw->ctrl %0x\n", xip_ctrl_hw->ctrl);
+        for (uint32 i = 0; i < FLASH_PAGE_SIZE; i++) { // 256-byte Aligned Page
+          array_int[i] = rand();
+        }
+        uint32 from_time = time_us_32();
+        __dsb();
+        flash_range_erase(free_start_offset, FLASH_SECTOR_SIZE);
+        __dsb();
+        flash_range_program(free_start_offset, array_int, FLASH_PAGE_SIZE);
+        __dsb();
+        qspi_flash_debug_time = time_us_32() - from_time;
+        printf("@main 10 - qspi_flash_debug_time %d\n", qspi_flash_debug_time);
+        for (uint32 i = 0; i < FLASH_PAGE_SIZE / 4; i++) {
+          printf("free_start %d %0x\n", i, free_start[i]);
+        }
+        sleep_ms(1000);
     }
 }
