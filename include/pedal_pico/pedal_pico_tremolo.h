b46c33aea0dd008f46b43f7ba43280a4e36de42f
Kenta Ishii <k.i.drivingroad21@gmail.com>
2021-06-25 20:01:26 +0900

    1. Modified README.md about Pedal
    2. Modified pedal_pico_tremolo
     a. Added Fade-in to Output Mode

--- include/pedal_pico/pedal_pico_tremolo.h
+++ include/pedal_pico/pedal_pico_tremolo.h
@@ -33,7 +33,7 @@
 extern "C" {
 #endif
 
-#define PEDAL_PICO_TREMOLO_OSC_TRIANGLE_1_TIME_MULTIPLIER 36
+#define PEDAL_PICO_TREMOLO_OSC_TRIANGLE_1_TIME_MULTIPLIER 108
 #define PEDAL_PICO_TREMOLO_OSC_START_HYSTERESIS_SHIFT 1 // Divide by 2
 #define PEDAL_PICO_TREMOLO_OSC_START_THRESHOLD_MULTIPLIER 1 // From -66.22dB (Loss 2047) to -36.39dB (Loss 66) in ADC_VREF (Typically 3.3V)
 #define PEDAL_PICO_TREMOLO_OSC_START_COUNT_MAX 2000 // 28125 Divided by 2000 = Approx. 14Hz
@@ -46,6 +46,7 @@ volatile uint16_t pedal_pico_tremolo_osc_speed;
 volatile bool pedal_pico_tremolo_osc_is_negative;
 volatile int8_t pedal_pico_tremolo_osc_start_threshold;
 volatile uint16_t pedal_pico_tremolo_osc_start_count;
+volatile bool pedal_pico_tremolo_osc_is_faded;
 
 void pedal_pico_tremolo_set();
 void pedal_pico_tremolo_process(int32_t normalized_1, uint16_t conversion_2, uint16_t conversion_3, uint8_t sw_mode);
--- lib/pedal_pico/pedal_pico_tremolo/pedal_pico_tremolo.c
+++ lib/pedal_pico/pedal_pico_tremolo/pedal_pico_tremolo.c
@@ -21,6 +21,7 @@ void pedal_pico_tremolo_set() {
     pedal_pico_tremolo_osc_is_negative = false;
     pedal_pico_tremolo_osc_start_threshold = (int8_t)((pedal_pico_tremolo_conversion_3 >> UTIL_PEDAL_PICO_ADC_SHIFT) * PEDAL_PICO_TREMOLO_OSC_START_THRESHOLD_MULTIPLIER); // Make 5-bit Value (0-31) and Multiply
     pedal_pico_tremolo_osc_start_count = 0;
+    pedal_pico_tremolo_osc_is_faded = false;
 }
 
 void pedal_pico_tremolo_process(int32_t normalized_1, uint16_t conversion_2, uint16_t conversion_3, uint8_t sw_mode) {
@@ -60,6 +61,7 @@ void pedal_pico_tremolo_process(int32_t normalized_1, uint16_t conversion_2, uin
     if (pedal_pico_tremolo_osc_start_count == 0) {
         pedal_pico_tremolo_osc_triangle_1_index = 0;
         pedal_pico_tremolo_osc_is_negative = false;
+        pedal_pico_tremolo_osc_is_faded = false;
     }
     /* Get Oscillator */
     int32_t fixed_point_value_triangle_1 = util_pedal_pico_table_triangle_1[abs(pedal_pico_tremolo_osc_triangle_1_index) / PEDAL_PICO_TREMOLO_OSC_TRIANGLE_1_TIME_MULTIPLIER]; // Depending on osc_spped, the index value may have a negative value.
@@ -67,6 +69,7 @@ void pedal_pico_tremolo_process(int32_t normalized_1, uint16_t conversion_2, uin
     if (pedal_pico_tremolo_osc_triangle_1_index >= UTIL_PEDAL_PICO_OSC_TRIANGLE_1_TIME_MAX * PEDAL_PICO_TREMOLO_OSC_TRIANGLE_1_TIME_MULTIPLIER) {
         pedal_pico_tremolo_osc_triangle_1_index = (UTIL_PEDAL_PICO_OSC_TRIANGLE_1_TIME_MAX * PEDAL_PICO_TREMOLO_OSC_TRIANGLE_1_TIME_MULTIPLIER) - 1;
         pedal_pico_tremolo_osc_is_negative = true;
+        if (! pedal_pico_tremolo_osc_is_faded) pedal_pico_tremolo_osc_is_faded = true;
     } else if (pedal_pico_tremolo_osc_triangle_1_index <= 0) {
         pedal_pico_tremolo_osc_triangle_1_index = 0;
         pedal_pico_tremolo_osc_is_negative = false;
@@ -76,12 +79,18 @@ void pedal_pico_tremolo_process(int32_t normalized_1, uint16_t conversion_2, uin
      * In the calculation, we extend the value to 64-bit signed integer because of the overflow from the 32-bit space.
      * In the multiplication to get only the integer part, 32-bit arithmetic shift left is needed at the end because we have had two 16-bit decimal part in each value.
      */
-    if (sw_mode == 1) {
-        fixed_point_value_triangle_1 >>= 2;
-    } else if (sw_mode == 0) {
+    if (sw_mode == 1) { // Inverted Shallow
         fixed_point_value_triangle_1 >>= 1;
+    } else if (sw_mode == 2) { // Fade In
+        if (pedal_pico_tremolo_osc_is_faded) {
+            fixed_point_value_triangle_1 = 0x00010000;
+        }
     }
-    normalized_1 = (int32_t)((((int64_t)normalized_1 << 16) * (int64_t)(0x00010000 - fixed_point_value_triangle_1)) >> 32);
+    if (sw_mode == 1) {
+        normalized_1 = (int32_t)((((int64_t)normalized_1 << 16) * (int64_t)(0x00010000 - fixed_point_value_triangle_1)) >> 32);
+    } else {
+        normalized_1 = (int32_t)((((int64_t)normalized_1 << 16) * (int64_t)fixed_point_value_triangle_1) >> 32);
+    }
     pedal_pico_tremolo->output_1 = normalized_1;
     pedal_pico_tremolo->output_1_inverted = -normalized_1;
 }
