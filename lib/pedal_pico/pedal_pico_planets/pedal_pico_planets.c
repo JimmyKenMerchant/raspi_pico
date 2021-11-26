f804eca354514f2d594d4214e1d4c7955a58bc44
Kenta Ishii <k.i.drivingroad21@gmail.com>
2021-11-26 18:51:31 +0900

    1. Modified Library, pedal_pico_planets  a. To Prevent Possible Noise on Changing Regular Mode to Band-pass Filter Mode  b. Stand-alone Low-pass Filter Mode, but Not High-pass Filter Mode that Can Be Functioned on Regular Mode
    2. Modified README.md about Pedal

--- lib/pedal_pico/pedal_pico_planets/pedal_pico_planets.c
+++ lib/pedal_pico/pedal_pico_planets/pedal_pico_planets.c
@@ -21,7 +21,7 @@ void pedal_pico_planets_set() {
     uint16_t delay_time_high_pass = ((UTIL_PEDAL_PICO_ADC_FINE_RESOLUTION + 1) - (pedal_pico_planets_conversion_2 >> UTIL_PEDAL_PICO_ADC_FINE_SHIFT)) << PEDAL_PICO_PLANETS_DELAY_TIME_SHIFT; // Make 6-bit Value (64-1) and Shift
     pedal_pico_planets_delay_time_high_pass = delay_time_high_pass;
     pedal_pico_planets_delay_time_high_pass_interpolation = delay_time_high_pass;
-    uint16_t delay_time_low_pass = _max(((((pedal_pico_planets_conversion_3 >> UTIL_PEDAL_PICO_ADC_FINE_SHIFT) + 1) << PEDAL_PICO_PLANETS_DELAY_TIME_SHIFT) >> 1), 1); // Make 6-bit Value (1-64) and Shift, then Halve with 1 at Minimum (1-32)
+    uint16_t delay_time_low_pass = _max((((pedal_pico_planets_conversion_3 >> UTIL_PEDAL_PICO_ADC_FINE_SHIFT) + 1) << PEDAL_PICO_PLANETS_DELAY_TIME_SHIFT) >> 1, 1); // Make 6-bit Value (1-64) and Shift, then Halve with 1 at Minimum (1-32)
     pedal_pico_planets_delay_time_low_pass = delay_time_low_pass;
     pedal_pico_planets_delay_time_low_pass_interpolation = delay_time_low_pass;
     pedal_pico_planets_moving_average_sum_low_pass = 0;
@@ -34,13 +34,13 @@ void pedal_pico_planets_process(int32_t normalized_1, uint16_t conversion_2, uin
     }
     if (abs(conversion_3 - pedal_pico_planets_conversion_3) >= UTIL_PEDAL_PICO_ADC_FINE_THRESHOLD) {
         pedal_pico_planets_conversion_3 = conversion_3;
-        pedal_pico_planets_delay_time_low_pass = _max(((((pedal_pico_planets_conversion_3 >> UTIL_PEDAL_PICO_ADC_FINE_SHIFT) + 1) << PEDAL_PICO_PLANETS_DELAY_TIME_SHIFT) >> 1), 1); // Make 6-bit Value (1-64) and Shift, then Halve with 1 at Minimum (1-32)
+        pedal_pico_planets_delay_time_low_pass = _max((((pedal_pico_planets_conversion_3 >> UTIL_PEDAL_PICO_ADC_FINE_SHIFT) + 1) << PEDAL_PICO_PLANETS_DELAY_TIME_SHIFT) >> 1, 1); // Make 6-bit Value (1-64) and Shift, then Halve with 1 at Minimum (1-32)
+    }
+    if (sw_mode == 1) {
+        pedal_pico_planets_delay_time_low_pass = _max(pedal_pico_planets_delay_time_high_pass >> 1, 1); // Halve with 1 at Minimum (1-32)
     }
     pedal_pico_planets_delay_time_high_pass_interpolation = _interpolate(pedal_pico_planets_delay_time_high_pass_interpolation, pedal_pico_planets_delay_time_high_pass, PEDAL_PICO_PLANETS_DELAY_TIME_INTERPOLATION_ACCUM_FIXED_1);
     pedal_pico_planets_delay_time_low_pass_interpolation = _interpolate(pedal_pico_planets_delay_time_low_pass_interpolation, pedal_pico_planets_delay_time_low_pass, PEDAL_PICO_PLANETS_DELAY_TIME_INTERPOLATION_ACCUM_FIXED_1);
-    if (sw_mode == 1) {
-        pedal_pico_planets_delay_time_low_pass_interpolation = pedal_pico_planets_delay_time_high_pass_interpolation;
-    }
     /* High-pass Filter */
     int16_t delay_y = pedal_pico_planets_delay_y[((pedal_pico_planets_delay_y_index + PEDAL_PICO_PLANETS_DELAY_TIME_MAX) - pedal_pico_planets_delay_time_high_pass_interpolation) % PEDAL_PICO_PLANETS_DELAY_TIME_MAX];
     /* First Stage: High Pass Filter */
@@ -54,18 +54,19 @@ void pedal_pico_planets_process(int32_t normalized_1, uint16_t conversion_2, uin
     pedal_pico_planets_delay_y_index++;
     if (pedal_pico_planets_delay_y_index >= PEDAL_PICO_PLANETS_DELAY_TIME_MAX) pedal_pico_planets_delay_y_index -= PEDAL_PICO_PLANETS_DELAY_TIME_MAX;
     /* Second Stage: Low Pass Filter */
+    int32_t second_stage_1;
+    if (sw_mode == 1) {
+        second_stage_1 = high_pass_1;
+    } else if (sw_mode == 2) {
+        second_stage_1 = normalized_1;
+    } else {
+        second_stage_1 = high_pass_1;
+    }
     int32_t low_pass_1;
     pedal_pico_planets_moving_average_sum_low_pass -= (pedal_pico_planets_moving_average_sum_low_pass / (int32_t)pedal_pico_planets_delay_time_low_pass_interpolation);
-    pedal_pico_planets_moving_average_sum_low_pass += high_pass_1;
+    pedal_pico_planets_moving_average_sum_low_pass += second_stage_1;
     low_pass_1 = pedal_pico_planets_moving_average_sum_low_pass / (int32_t)pedal_pico_planets_delay_time_low_pass_interpolation;
-    int32_t mixed_1;
-    if (sw_mode == 1) {
-        mixed_1 = low_pass_1;
-    } else if (sw_mode == 2) {
-        mixed_1 = high_pass_1;
-    } else {
-        mixed_1 = low_pass_1;
-    }
+    int32_t mixed_1 = low_pass_1;
     /* Output */
     pedal_pico_planets->output_1 = mixed_1;
     pedal_pico_planets->output_1_inverted = -mixed_1;
